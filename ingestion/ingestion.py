73f0a2972cfed0b37ef7ec4e6e16366804c32a57
Gus Ostow <augustus.ostow@generalassemb.ly>
2018-10-29 18:34:42 -0700

    Fix "invalid start byte bug and pep8 formatting

--- ingestion/ingestion.py
+++ ingestion/ingestion.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
-import sys
 import pika
 import json
 import os
 import time
 from utils import parse_log, is_get_request
 
-#Connect  to RabbitMQ
-credentials = pika.PlainCredentials(os.environ['RABBITMQ_DEFAULT_USER'], os.environ['RABBITMQ_DEFAULT_PASS'])
+# Connect  to RabbitMQ
+credentials = pika.PlainCredentials(
+    os.environ['RABBITMQ_DEFAULT_USER'], os.environ['RABBITMQ_DEFAULT_PASS'])
 parameters = pika.ConnectionParameters(host='rabbit',
                                        port=5672, credentials=credentials)
 
@@ -29,26 +29,25 @@ channel.queue_declare(queue='log-analysis')
 
 # Read weblogs
 
-f = open('weblogs/weblogs.log', 'r')
+with open('weblogs/weblogs.log', 'r') as f:
+    while True:
+        try:
+            msg = f.readline()
 
-while True:
-    try:
-        msg = f.readline()
+            if not msg:
+                break
+            # If message is GET request, ingest it into the queue
+            if is_get_request(msg):
+                # Parse GET request for relevant information
+                day, status, source = parse_log(msg)
 
-        if not msg:
-            break
-        #If message is GET request, ingest it into the queue
-        if is_get_request(msg):
-            # Parse GET request for relevant information
-            day, status, source = parse_log(msg)
+                # Store in RabbitMQ
+                body = json.dumps({'day': str(day), 'status': status})
+                channel.basic_publish(exchange='',
+                                      routing_key='log-analysis',
+                                      body=body)
 
-            # Store in RabbitMQ
-            body = json.dumps({'day': str(day), 'status': status})
-            channel.basic_publish(exchange='',
-                                  routing_key='log-analysis',
-                                  body=body)
+        except Exception as e:
+            print(f'Unexpected error: {e}')
 
-    except Exception as e:
-        print(f"Unexpected error: {e}")
-
-connection.close()
+    connection.close()
